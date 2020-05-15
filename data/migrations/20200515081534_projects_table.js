29ea85b70a9370b45edd67e757c4acc13464ec96
Pinefoot <jameslcarpino@gmail.com>
2020-05-15 08:41:03 -0700

    set up project, wrote out migration.

---
+++ data/migrations/20200515081534_projects_table.js
@@ -0,0 +1,55 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl =>{
+        tbl.increments();
+        tbl.string('proj_name', 128).notNullable().unique()
+        tbl.string('proj_description', 128)
+        tbl.boolean('completed').defaultTo(false)
+    })
+    .createTable('tasks', tbl=>{
+        tbl.increments();
+        tbl.string('task_name', 128)
+        tbl.string('task_description').notNullable()
+        tbl.string('task_notes', 128)
+        tbl.boolean('completed').defaultTo(false)
+        tbl.integer('proj_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+    .createTable('resources', tbl=>{
+        tbl.increments();
+        tbl.string('resources_name', 128).notNullable().unique()
+        tbl.string('resources_description', 128)
+        tbl.boolean('in_use').defaultTo(false)
+
+    })
+    .createTable('project_resources', tbl=>{
+        tbl.increments();
+        //foreign key that connects to the projects table
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        //foreign key that connects to the resources table
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('resources')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    
+};
